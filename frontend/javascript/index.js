d11b36c6229f0b494b80d7c0b2a2d931051f3c63
Jared White <jared@jaredwhite.com>
2021-08-01 22:03:43 -0700

    Additional style tweaks

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -4,14 +4,3 @@ import TumblrPhotos from "./tumblr_photos.js"
 // Import all javascript files from src/_components
 const componentsContext = require.context("bridgetownComponents", true, /.js$/)
 componentsContext.keys().forEach(componentsContext)
-
-window.addEventListener("DOMContentLoaded", () => {
-  let mouseMoved = false
-
-  document.body.addEventListener("mousemove", () => {
-    if (!mouseMoved) {
-      mouseMoved = true
-      setTimeout(() => document.querySelector("main > aside").classList.add("faded"), 1000)
-    }
-  })
-})
