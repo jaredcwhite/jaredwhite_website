ef36196e4e116d19797b8ec46f216e08ef3cf3b6
Jared White <jared@whitefusion.studio>
2021-09-19 09:34:53 -0700

    Guard from nil

--- plugins/builders/helpers_builder.rb
+++ plugins/builders/helpers_builder.rb
@@ -6,7 +6,7 @@ class HelpersBuilder < SiteBuilder
 
   def hashtags(input, full_url: false)
     tag_url_prefix = "#{full_url ? site.config.url.to_s : ""}/tag/"      
-    input.to_s.gsub(/(^|\s|\>)#([a-z\d-]+)/i, "\\1<a href=\"#{tag_url_prefix}#{$2.downcase}\" class=\"hashtag\">#\\2</a>").html_safe
+    input.to_s.gsub(/(^|\s|\>)#([a-z\d-]+)/i, "\\1<a href=\"#{tag_url_prefix}#{$2&.downcase}\" class=\"hashtag\">#\\2</a>").html_safe
   end
 
   def translate_title(input)
