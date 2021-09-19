3746eba740db66f0a8e2f7db8fa0891d0b351beb
Jared White <jared@jaredwhite.com>
2021-09-19 10:12:40 -0700

    fix hashtags gsub

--- plugins/builders/helpers_builder.rb
+++ plugins/builders/helpers_builder.rb
@@ -6,7 +6,9 @@ class HelpersBuilder < SiteBuilder
 
   def hashtags(input, full_url: false)
     tag_url_prefix = "#{full_url ? site.config.url.to_s : ""}/tag/"      
-    input.to_s.gsub(/(^|\s|\>)#([a-z\d-]+)/i, "\\1<a href=\"#{tag_url_prefix}#{Regexp.last_match(2).downcase}\" class=\"hashtag\">#\\2</a>").html_safe
+    input.to_s.gsub(/(^|\s|\>)#([a-z\d-]+)/i) do
+      "#{Regexp.last_match(1)}<a href=\"#{tag_url_prefix}#{Regexp.last_match(2).downcase}\" class=\"hashtag\">##{Regexp.last_match(2)}</a>"
+    end.html_safe
   end
 
   def translate_title(input)
