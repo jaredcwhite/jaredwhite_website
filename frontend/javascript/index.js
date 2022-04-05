4b88ee5ecbcda19989b563e5548c93307d51b5c9
Jared White <jared@jaredwhite.com>
2022-04-05 10:40:59 -0700

    Switch to Glass

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -7,7 +7,7 @@ import smoothscroll from "smoothscroll-polyfill"
 smoothscroll.polyfill()
 
 // Ruby2JS scripts
-import "./tumblr_photos.js.rb"
+import "./glass_photos.js.rb"
 import "./shoelace.js.rb"
 import "./dark_mode.js.rb"
 import "./sidebar.js.rb"
