8d67d557e2c3948723a8189825195682747300c9
jaredcwhite <jared@whitefusion.studio>
2023-02-06 23:06:52 +0000

    Get redis working

--- config/initializers.rb
+++ config/initializers.rb
@@ -1,5 +1,7 @@
 Bridgetown.configure do |config|
   init :dotenv
   init :ssr
+
+  ENV["REDIS_URL"] = "redis://#{ENV['REDIS_HOSTPORT']}" if ENV["REDIS_HOSTPORT"]
   init :toottown, access_token: ENV.fetch("TOOTTOWN_ACCESS_TOKEN"), instance_url: ENV.fetch("TOOTTOWN_INSTANCE_URL")
 end
