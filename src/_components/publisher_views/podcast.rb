adb10b06d356e9830b9256445971b913d85a4271
jaredcwhite <jared@whitefusion.studio>
2023-04-04 18:06:26 +0000

    Finalize podcast editor

--- src/_components/publisher_views/podcast.rb
+++ src/_components/publisher_views/podcast.rb
@@ -4,5 +4,9 @@ class PublisherViews::Podcast < Bridgetown::Component
   def initialize(request:)
     @r = request
     @site = Bridgetown::Current.site
+
+    site.collections.podcast.read if site.collections.podcast.resources.blank?
+
+    @new_number = site.collections.podcast.resources.first.data.number.to_i + 1
   end
 end
