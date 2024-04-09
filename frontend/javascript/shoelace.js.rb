cefa23695df052ada6644a4d0793c9c4f531426f
Jared White <jared@jaredwhite.com>
2024-04-08 23:55:37 -0700

    update remix version

--- frontend/javascript/shoelace.js.rb
+++ frontend/javascript/shoelace.js.rb
@@ -10,7 +10,7 @@ register_icon_library(
   resolver: -> (name) do
     match = name.match /^(.*?)\/(.*?)$/
     match[1] = match[1].char_at(0).upcase() + match[1].slice(1)
-    "https://cdn.jsdelivr.net/npm/remixicon@2.5.0/icons/#{match[1]}/#{match[2]}.svg"
+    "https://cdn.jsdelivr.net/npm/remixicon@4.2.0/icons/#{match[1]}/#{match[2]}.svg"
   end,
   mutator: -> svg { svg.set_attribute :fill, :current_color }
 )
