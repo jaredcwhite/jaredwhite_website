3ca08ebb98eb91c3148b9881623afca773b7f0fa
Jared White <jared@jaredwhite.com>
2024-04-08 23:50:23 -0700

    Add Bluesky

--- config/initializers.rb
+++ config/initializers.rb
@@ -1,5 +1,7 @@
 Bridgetown.configure do |config|
   init :dotenv
   init :ssr
-  init :toottown, access_token: ENV.fetch("TOOTTOWN_ACCESS_TOKEN"), instance_url: ENV.fetch("TOOTTOWN_INSTANCE_URL")
+  if ENV.fetch("TOOTTOWN_ACCESS_TOKEN", nil)
+    init :toottown, access_token: ENV.fetch("TOOTTOWN_ACCESS_TOKEN"), instance_url: ENV.fetch("TOOTTOWN_INSTANCE_URL")
+  end
 end
